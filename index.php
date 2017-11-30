2e7e18b9a3f4b73b878af442aa990e2d4b48cbff
Sunthon <comsc511463041@gmail.com>
2017-11-30 14:22:49 +0700

    Update index.php
    Fix access token.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // echo 'Hello new world.';
 
-// $access_token = 'nLV5oqCjlBiKJsoV9HASlhX4lyNkfi5ZcfiCRS+o0hDJ0LdS6Da0SvVIgLYvgnbu4Ughe1v6PCRVxRyNdlw5uq53R5QehyWx2VBGuv/bL+WwTZRsBL2mORUjiZ+F5CMUnUe3Jp8QmSgHEc2cMW6rNQdB04t89/1O/w1cDnyilFU=';
+ $access_token = 'nLV5oqCjlBiKJsoV9HASlhX4lyNkfi5ZcfiCRS+o0hDJ0LdS6Da0SvVIgLYvgnbu4Ughe1v6PCRVxRyNdlw5uq53R5QehyWx2VBGuv/bL+WwTZRsBL2mORUjiZ+F5CMUnUe3Jp8QmSgHEc2cMW6rNQdB04t89/1O/w1cDnyilFU=';
 
 // $url = 'https://api.line.me/v1/oauth/verify';
 
